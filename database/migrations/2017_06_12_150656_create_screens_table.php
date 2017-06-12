e797d3c78af7cfa6b67a7d03a993c83a1df42343
VienLe93 <lecongvien.dct@gmail.com>
2017-06-12 22:22:26 +0700

    create screen migration

---
+++ database/migrations/2017_06_12_150656_create_screens_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateScreensTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('screens', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('screen_tech');
+            $table->string('resolution');
+            $table->integer('with_screen');
+            $table->string('touch_screen');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('screens');
+    }
+}
