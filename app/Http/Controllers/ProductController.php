2b414032a961a1e444703fba97874cfe1156fb3b
VienLe93 <lecongvien.dct@gmail.com>
2017-06-21 13:14:29 +0700

    show new product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -13,7 +13,13 @@ class ProductController extends Controller
 {
     public function index()
     {
-		$datas = DB::table('products')->join('opera_systems', 'opera_systems.id', '=', 'products.opera_system_id')->join('connects', 'connects.id', '=', 'products.connect_id')->join('batteries', 'batteries.id', '=', 'products.battery_id')->join('front_cameras', 'front_cameras.id', '=', 'products.front_camera_id')->join('back_cameras', 'back_cameras.id', '=', 'products.back_camera_id')->join('memories', 'memories.id', '=', 'products.memory_id')->get();
+        $datas = DB::table('products')
+        ->join('opera_systems', 'opera_systems.id', '=', 'products.opera_system_id')
+        ->join('connects', 'connects.id', '=', 'products.connect_id')
+        ->join('batteries', 'batteries.id', '=', 'products.battery_id')
+        ->join('front_cameras', 'front_cameras.id', '=', 'products.front_camera_id')
+        ->join('back_cameras', 'back_cameras.id', '=', 'products.back_camera_id')
+        ->join('memories', 'memories.id', '=', 'products.memory_id')->get();
         return view('products.show_new_product')->with('data', $datas);
     }
 }
--- app/Product.php
+++ app/Product.php
@@ -11,19 +11,23 @@ class Product extends Model
     protected $table = 'products';
     protected $fillable = ['name', 'slug', 'price', 'description', 'image', 'category_id', 'factory_id', 'tophot', 'warranty_moth', 'status', 'back_camera_id', 'front_camera_id', 'battery_id', 'connect_id', 'design_id', 'opera_system_id', 'screen_id',  'utility_id'];
 
-    public function front_cameras(){
-    	return $this->hasOne('App\Front_Camera');
+    public function front_cameras()
+    {
+        return $this->hasOne('App\FrontCamera');
     }
 
-    public function front_camera(){
-    	return $this->belongsTo('App\Front_Camera');
+    public function front_camera()
+    {
+        return $this->belongsTo('App\FrontCamera');
     }
 
-    public function back_cameras(){
-    	return $this->hasOne('App\Back_Camera');
+    public function back_cameras()
+    {
+        return $this->hasOne('App\BackCamera');
     }
 
-    public function back_camera(){
-    	return $this->belongsTo('App\Back_Camera');
+    public function back_camera()
+    {
+        return $this->belongsTo('App\BackCamera');
     }
 }
