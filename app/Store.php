20b9e9f45574e5d881da6d28471c27efb07b03d5
VienLe93 <lecongvien.dct@gmail.com>
2017-06-23 19:34:16 +0700

    show detail product

--- app/Store.php
+++ app/Store.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Store extends Model
 {
-    	
+    protected $table = 'stores';
+
+    public function products()
+    {
+        return $this->hasOne('App\Product');
+    }
+
+    public function product()
+    {
+        return $this->belongsTo('App\Product');
+    }
 }
