ee36b1480f70b1c584247fef50d67821cffa23e2
LuongVanTien <luongvantiendt13ctt01@gmail.com>
2017-06-23 19:12:08 +0700

    create news model and list news model

---
+++ app/News.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class News extends Model
+{
+    protected $table = "news";
+    protected $fillable = [
+        'title', 'slug', 'list_new_id', 'description', 'content', 'image'
+    ];
+   	public function listNew()
+    {
+    	return $this->belongsTo('App\ListNew');
+    }
+    public function user()
+    {
+    	return $this->belongsTo('App\User');
+    }
+}
