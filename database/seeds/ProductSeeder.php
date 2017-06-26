b97cdec222b3870f529d3659e2b3b61ad5a32b13
VienLe93 <lecongvien.dct@gmail.com>
2017-06-26 16:01:00 +0700

    update product seeder

--- database/seeds/ProductSeeder.php
+++ database/seeds/ProductSeeder.php
@@ -12,8 +12,8 @@ class ProductSeeder extends Seeder
     public function run()
     {
         DB::table('products')->insert([
-            ['name' => "SamSung S8+", 'slug' => "Sam-Sung-S8-Plus", 'price' => "20490000", 'description' => "SamSung S8+", 'image' => "ảnh", 'tophot' => "1", 'warranty_moth' => "12", 'status' => true, 'category_id' => "1", 'factory_id' => "1", 'back_camera_id' => "1", 'front_camera_id' => "1", 'battery_id' => "1", 'connect_id' => "1", 'design_id' => "1", 'opera_system_id' => "1", 'screen_id' => "1", 'utility_id' => "1", 'color_id' => "1", 'memory_id' => "1"],
-            ['name' => "Iphone 7+", 'slug' => "Iphone-7-Plus", 'price' => "2599000", 'description' => "Iphone 7+", 'image' => "ảnh", 'tophot' => "1", 'warranty_moth' => "12", 'status' => true, 'category_id' => "2", 'factory_id' => "2", 'back_camera_id' => "2", 'front_camera_id' => "2", 'battery_id' => "2", 'connect_id' => "2", 'design_id' => "2", 'opera_system_id' => "2", 'screen_id' => "2", 'utility_id' => "2", 'color_id' => "2", 'memory_id' => "2"],
+            ['name' => "SamSung S8+", 'slug' => "Sam-Sung-S8-Plus", 'price' => "20490000", 'description' => "SamSung S8+", 'image' => "ảnh", 'tophot' => "1", 'warranty_moth' => "12", 'status' => true, 'category_id' => "1", 'factory_id' => "1", 'back_camera_id' => "1", 'front_camera_id' => "1", 'battery_id' => "1", 'connect_id' => "1", 'design_id' => "1", 'opera_system_id' => "1", 'screen_id' => "1", 'utility_id' => "1", 'color_id' => "1", 'memory_id' => "1", 'vote_id' => "5"],
+            ['name' => "Iphone 7+", 'slug' => "Iphone-7-Plus", 'price' => "2599000", 'description' => "Iphone 7+", 'image' => "ảnh", 'tophot' => "1", 'warranty_moth' => "12", 'status' => true, 'category_id' => "2", 'factory_id' => "2", 'back_camera_id' => "2", 'front_camera_id' => "2", 'battery_id' => "2", 'connect_id' => "2", 'design_id' => "2", 'opera_system_id' => "2", 'screen_id' => "2", 'utility_id' => "2", 'color_id' => "2", 'memory_id' => "2", 'vote_id' => "1", 'vote_id' => "5"],
         ]);
     }
 }
