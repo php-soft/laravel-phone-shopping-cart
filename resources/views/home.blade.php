834611652e70b11a0adfbf197df82f58933c367a
VienLe93 <lecongvien.dct@gmail.com>
2017-06-28 00:17:51 +0700

    update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,27 @@
 
 @section('content')
 <div class="container">
-    @foreach($data as $row)
+    @foreach($products as $product)
         <div class="row">
             <div class="col-sm-6 col-md-4">
-                <a href="{{url('products/' .$row->slug)}}">
+                <a href="{{url('products/' .$product->slug)}}">
                     <div class="thumbnail">
-                        <img class="img-responsive" src="{!!url('uploads/products/'.$row->image)!!}" alt="">
+                        <img class="img-responsive" src="{!!$product->image!!}" alt="">
                         <div class="caption">
-                            <h3>{!! $row->name !!}</h3>
+                            <h3>{!! $product->name !!}</h3>
                             <tr>
-                                <td><p>HDH: {!! $row->operaSystem->opera_system !!}</p></td>
-                                <td><p>CPU: {!! $row->operaSystem->chipset !!}</p></td>
-                                <td><p>RAM: {!! $row->memory->ram !!} GB, ROM: {!! $row->memory->rom !!} GB</p></td>
-                                <td><p>Camera: {!! $row->backCamera->resolution !!}, Selfie: {!! $row->frontCamera->resolution !!}</p></td>
-                                <td><p>PIN: {!! $row->battery->battery_capacity !!}, SIM: {!! $row->connect->sim !!}</p></td>
+                                <td><p>HDH: {!! $product->operaSystem->opera_system !!}</p></td>
+                                <td><p>CPU: {!! $product->operaSystem->chipset !!}</p></td>
+                                <td><p>RAM: {!! $product->memory->ram !!} GB, ROM: {!! $product->memory->rom !!} GB</p></td>
+                                <td><p>Camera: {!! $product->backCamera->resolution !!}, Selfie: {!! $product->frontCamera->resolution !!}</p></td>
+                                <td><p>PIN: {!! $product->battery->battery_capacity !!}, SIM: {!! $product->connect->sim !!}</p></td>
                             </tr>
-                            <h3>{!! $row->price !!}</h3>
+                            @if($product->sale_price === null)
+                                <h3>{!! $product->price !!} đ</h3>
+                            @else 
+                                <h3>{!! $product->sale_price !!} đ</h3>
+                                <h4><del>{!! $product->price !!} đ</del>&nbsp;&nbsp; -{!! number_format((($product->price-$product->sale_price)/$product->price)*100, 2) !!} %</h4>
+                            @endif
                         </div>
                     </div>
                 </a>
