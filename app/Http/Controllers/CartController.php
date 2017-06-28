fe785e1955de5e5a0473869dbc60fe0d7c171ffa
leducthien1995 <leducthien1995@gmail.com>
2017-06-28 09:12:04 +0700

    Create cart for customer

---
+++ app/Http/Controllers/CartController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Product;
+use Cart;
+use Session;
+
+class CartController extends Controller
+{
+    public function index()
+    {
+        if($stt=0){
+            return view('cart');
+        }
+        else{
+            $products = Product::all();
+            $content = Cart::content();
+            $total=Cart::total();
+            $qty=Cart::count();
+            $qtysp=Cart::content()->count();
+            $subtotal=Cart::subtotal();
+            return view('pages.cart',compact('qtysp'),compact('subtotal'))->with('products',$products)->with('content',$content)->with('total',$total)->with('qty',$qty);     
+        }           
+    }
+     public function getAddtoCart($id){
+            $stt=1;
+            $product = Product::find($id);
+            $insert = Cart::add(['id' => $product->id, 'name' => $product->name, 'qty' => 1, 'price' => $product->price, 'image' =>  $product->image]);
+            return redirect()->route('home');
+    }
+    public function getCheckout(){
+        $products = Product::all();
+        $content = Cart::content();
+        $total=Cart::total();
+        $qty=Cart::count();
+        $qtysp=Cart::content()->count();
+        $subtotal=Cart::subtotal();
+        return view('home',compact('content'),compact('products'),compact('qtysp'),compact('subtotal'))->with('total',$total)->with('qty',$qty);
+    }
+    public function getUpdateQtyCart($id){
+        $rowid=Cart::content()->where('id',$id)->pluck('rowId')->first();
+        $qty=Cart::content()->where('id',$id)->pluck('qty')->first();
+        Cart::update($rowid,$qty+1);
+        // $total=Cart::total();
+        return redirect()->back();
+    }
+    public function getDeleteQtyCart($id){
+        $rowid=Cart::content()->where('id',$id)->pluck('rowId')->first();
+        $qty=Cart::content()->where('id',$id)->pluck('qty')->first();
+        Cart::update($rowid,$qty-1);
+        // $total=Cart::total();
+        return redirect()->back();
+    }
+    public function getDeleteProductCart($id){
+        $rowid=Cart::content()->where('id',$id)->pluck('rowId')->first();
+        Cart::remove($rowid);
+        return redirect()->back();
+    }
+    public function getDeleteCart(){
+        $content=Cart::content()->pluck('rowId');
+        foreach($content as $item){
+            Cart::remove($item);
+        }
+        return redirect()->back();
+    }
+    public function getUpdateCart($id){
+        // $qty=Input::get('qty') ;
+        // dd($qty);
+        echo "đang hoàn thiện";
+    }   
+}
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Styles -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
@@ -46,6 +47,9 @@
                             <li><a href="{{ route('login') }}">Login</a></li>
                             <li><a href="{{ route('register') }}">Register</a></li>
                         @else
+                            <div class="pull-right">
+                                <a href="/gio-hang"><span class="glyphicon glyphicon-shopping-cart"> </span> Giỏ hàng</a>
+                            </div>
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                     {{ Auth::user()->name }} <span class="caret"></span>
@@ -70,7 +74,6 @@
                 </div>
             </div>
         </nav>
-
         @yield('content')
     </div>
 
