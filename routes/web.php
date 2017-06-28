9471cda05a80ce917f362a7b533ff15327e445d0
leducthien1995 <leducthien1995@gmail.com>
2017-06-28 09:39:22 +0700

    update customer cart

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,24 @@ Route::get('/', function () {
     return redirect('/home');
 });
 
+Route::get('/gio-hang','CartController@index')->name('show-cart');
+
+Route::get('/them-gio-hang/{product_id}','CartController@getAddToCart')->name('add-to-cart');
+
+Route::get('check-out',['uses'=>'CartController@getCheckout','as'=>'checkout']);
+
+Route::get('/gio-hang/cap-nhat-sp/{product_id}','CartController@getUpdateCart')->name('update-cart');
+
+Route::get('/gio-hang/cap-nhat/{product_id}','CartController@getUpdateQtyCart')->name('update-qty-cart');
+
+Route::get('/gio-hang/xoa-cap-nhat/{product_id}','CartController@getDeleteQtyCart')->name('delete-qty-cart');
+
+Route::get('/gio-hang/xoa-san-pham/{product_id}','CartController@getDeleteProductCart')->name('delete-product-cart');
+
+Route::get('/gio-hang/xoa-gio-hang','CartController@getDeleteCart')->name('delete-cart');
+
+Route::get('/dat-hang','OrderController@showOrder')->name('dat-hang');
+
 Route::get('/demoadmin', function () {
     return view('admin.demoadmin');
 });
@@ -22,9 +40,19 @@ Route::get('/demoadmin', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
 Route::group(['prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => ['auth']], function () {
+    
     Route::get('/orders', 'OrderController@index')->name('adminOrders');
+    
     Route::get('/orders/edit/{order_id}', 'OrderController@edit')->name('adminEditOrder');
+    
     Route::put('/orders/edit/{order_id}', 'OrderController@update')->name('adminUpdateOrder');
+    
     Route::get('/orders/delete/{order_id}', 'OrderController@destroy')->name('adminDeleteOrder');
 });
+
+// View::composer('*', function ($view) {
+//     $view->products = Cart::content();
+//     $view->totalPrice = Cart::subtotal();
+// });
