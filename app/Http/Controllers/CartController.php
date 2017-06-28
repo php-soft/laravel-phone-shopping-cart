bc83316094d6cb0640346b1c2567f9e9c7cec727
leducthien1995 <leducthien1995@gmail.com>
2017-06-28 09:32:01 +0700

    update customer cart

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -9,65 +9,74 @@ use Session;
 
 class CartController extends Controller
 {
-    public function index()
-    {
-        if($stt=0){
-            return view('cart');
-        }
-        else{
+    public function index () {
+        if ( $stt = 0 ) {
+            return view('cart'); 
+        } 
+        else {
             $products = Product::all();
             $content = Cart::content();
-            $total=Cart::total();
-            $qty=Cart::count();
-            $qtysp=Cart::content()->count();
-            $subtotal=Cart::subtotal();
-            return view('pages.cart',compact('qtysp'),compact('subtotal'))->with('products',$products)->with('content',$content)->with('total',$total)->with('qty',$qty);     
-        }           
+            $total = Cart::total();
+            $qty = Cart::count();
+            $qtysp = Cart::content()->count();
+            $subtotal = Cart::subtotal();
+            return view('pages.cart', compact('qtysp'), compact('subtotal'))->with('products', $products)
+                ->with('content', $content)->with('total', $total)->with('qty', $qty);     
+        }      
     }
-     public function getAddtoCart($id){
+
+     public function getAddtoCart ($id) {
             $stt=1;
             $product = Product::find($id);
-            $insert = Cart::add(['id' => $product->id, 'name' => $product->name, 'qty' => 1, 'price' => $product->price, 'image' =>  $product->image]);
+            $insert = Cart::add(['id' => $product->id, 'name' => $product->name, 'qty' => 1,
+             'price' => $product->price, 'image' =>  $product->image]);
             return redirect()->route('home');
     }
-    public function getCheckout(){
+
+    public function getCheckout () {
         $products = Product::all();
         $content = Cart::content();
-        $total=Cart::total();
-        $qty=Cart::count();
-        $qtysp=Cart::content()->count();
-        $subtotal=Cart::subtotal();
-        return view('home',compact('content'),compact('products'),compact('qtysp'),compact('subtotal'))->with('total',$total)->with('qty',$qty);
+        $total = Cart::total();
+        $qty = Cart::count();
+        $qtysp = Cart::content()->count();
+        $subtotal = Cart::subtotal();
+        return view('home', compact('content'), compact('products'), compact('qtysp'), compact('subtotal'))
+        ->with('total', $total)->with('qty', $qty);
     }
-    public function getUpdateQtyCart($id){
-        $rowid=Cart::content()->where('id',$id)->pluck('rowId')->first();
-        $qty=Cart::content()->where('id',$id)->pluck('qty')->first();
-        Cart::update($rowid,$qty+1);
+
+    public function getUpdateQtyCart ($id) {
+        $rowid = Cart::content()->where('id', $id)->pluck('rowId')->first();
+        $qty = Cart::content()->where('id', $id)->pluck('qty')->first();
+        Cart::update($rowid, $qty+1);
         // $total=Cart::total();
         return redirect()->back();
     }
-    public function getDeleteQtyCart($id){
-        $rowid=Cart::content()->where('id',$id)->pluck('rowId')->first();
-        $qty=Cart::content()->where('id',$id)->pluck('qty')->first();
-        Cart::update($rowid,$qty-1);
+
+    public function getDeleteQtyCart ($id) {
+        $rowid = Cart::content()->where('id', $id)->pluck('rowId')->first();
+        $qty = Cart::content()->where('id', $id)->pluck('qty')->first();
+        Cart::update($rowid, $qty-1);
         // $total=Cart::total();
         return redirect()->back();
     }
-    public function getDeleteProductCart($id){
-        $rowid=Cart::content()->where('id',$id)->pluck('rowId')->first();
+
+    public function getDeleteProductCart ($id) {
+        $rowid = Cart::content()->where('id', $id)->pluck('rowId')->first();
         Cart::remove($rowid);
         return redirect()->back();
     }
+
     public function getDeleteCart(){
-        $content=Cart::content()->pluck('rowId');
-        foreach($content as $item){
+        $content = Cart::content()->pluck('rowId');
+        foreach ($content as $item) {
             Cart::remove($item);
         }
         return redirect()->back();
     }
-    public function getUpdateCart($id){
-        // $qty=Input::get('qty') ;
+
+    public function getUpdateCart ($id) {
+        // $qty=Input::get('qty');
         // dd($qty);
         echo "đang hoàn thiện";
-    }   
+    }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,26 +1,25 @@
 @extends('layouts.app')
-
-@section('content')
-<div class="container">
-    @foreach($data as $row)
+@section('content')  
+    @foreach($products as $product)
         <div class="row">
             <div class="col-sm-6 col-md-4">
                 <a href="">
                     <div class="thumbnail">
-                        <img class="img-responsive" src="{!!url('uploads/products/'.$row->image)!!}" alt="">
+                        <img class="img-responsive" src="{!!url($product->image)!!}" alt="">
                         <div class="caption">
-                            <h3>{!! $row->name !!}</h3>
+                            <h3>{!! $product->name !!}</h3>
                             <tr>
-                                <td><p>HDH: {!! $row->operaSystem->opera_system !!}</p></td>
-                                <td><p>CPU: {!! $row->operaSystem->chipset !!}</p></td>
-                                <td><p>RAM: {!! $row->memory->ram !!} GB, ROM: {!! $row->memory->rom !!} GB</p></td>
-                                <td><p>Camera: {!! $row->backCamera->resolution !!}, Selfie: {!! $row->frontCamera->resolution !!}</p></td>
-                                <td><p>PIN: {!! $row->battery->battery_capacity !!}, SIM: {!! $row->connect->sim !!}</p></td>
+                                <td><p>HDH: {!! $product->operaSystem->opera_system !!}</p></td>
+                                <td><p>CPU: {!! $product->operaSystem->chipset !!}</p></td>
+                                <td><p>RAM: {!! $product->memory->ram !!} GB, ROM: {!! $product->memory->rom !!} GB</p></td>
+                                <td><p>Camera: {!! $product->backCamera->resolution !!}, Selfie: {!! $product->frontCamera->resolution !!}</p></td>
+                                <td><p>PIN: {!! $product->battery->battery_capacity !!}, SIM: {!! $product->connect->sim !!}</p></td>
                             </tr>
-                            <h3>{!! $row->price !!}</h3>
+                            <h3>{!! $product->price !!}</h3>
                         </div>
                     </div>
                 </a>
+                <a href="{{url('/them-gio-hang/'.$product->id)}}">Thêm giỏ hàng</a>
             </div>
         </div>
     @endforeach
